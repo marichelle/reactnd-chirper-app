9287becc7aa60d7a4659eb6faf9214adca1d74c5
Maya Estalilla <mayae@occasionbrands.com>
2020-04-02 19:52:39 -0400

    Add actions and reducers

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,10 @@
 import { getInitialData } from '../utils/api';
-import { receiveTweets } from './tweets';
-import { receiveUsers } from './users';
+import { receiveTweets } from '../actions/tweets';
+import { receiveUsers } from '../actions/users';
+import { setAuthorizedUser } from '../actions/authorizedUser';
+
+// hardcode authenticated user
+const AUTHORIZED_ID = 'sarah_edo';
 
 export function handleInitialData() {
   // redux thunk pattern
@@ -8,6 +12,7 @@ export function handleInitialData() {
     return getInitialData().then((tweets, users) => {
       dispatch(receiveTweets(tweets));
       dispatch(receiveUsers(users));
+      dispatch(setAuthorizedUser(AUTHORIZED_ID));
     });
   };
 }
